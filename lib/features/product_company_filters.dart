8d5a89efa2860c129dcfcba9ef15a319bccfbbd2
Danial-03-dev <danialzafarwork003@gmail.com>
2025-11-23 19:06:59 +0500

    feat: create ui for company filters feature

---
+++ lib/features/product_company_filters.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_app/widgets/custom_filter_chip.dart';
+
+class ProductCompanyFilters extends StatefulWidget {
+  const ProductCompanyFilters({super.key});
+
+  @override
+  State<ProductCompanyFilters> createState() => _ProductCompanyFiltersState();
+}
+
+class _ProductCompanyFiltersState extends State<ProductCompanyFilters> {
+  final List<String> filters = const ['All', 'Addidas', 'Bata', 'Nike'];
+  late String selectedFilter;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedFilter = filters[0];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 128,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: filters.length,
+        itemBuilder: (context, index) {
+          final filter = filters[index];
+
+          void handleFilter() {
+            setState(() {
+              selectedFilter = filter;
+            });
+          }
+
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: CustomFilterChip(
+              onTap: handleFilter,
+              label: filter,
+              selectedFilter: selectedFilter,
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
